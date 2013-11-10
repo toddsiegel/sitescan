e832acc828752ea184c838719a7e1c01befb287d
Todd Siegel <todd.siegel@solerasoftware.com>
2013-11-10 11:29:03 -0500

    more spec clean up
    add descriptive helpers for the different types of sites

--- spec/site_scan_spec.rb
+++ spec/site_scan_spec.rb
@@ -7,23 +7,33 @@ require_relative "../lib/sitescan/site"
 describe Site do
 
   it "can tell a site is not avialable" do
-    site = site_for "http://fasdfasdfasd.com"
-    site.wont_be :available?
+    unavailable_site.wont_be :available?
   end
 
   it "can tell a site is available" do
-    site = site_for "https://github.com"
-    site.must_be :available?
+    available_site.must_be :available?
   end
 
   it "is an unknown type of site" do
-    site = site_for "https://github.com"
-    site.must_be :unknown?
+    unknown_site.must_be :unknown?
   end
 
   private
 
+  def available_site
+    @available ||= site_for "https://github.com"
+  end
+
   def site_for(url)
     Site.new(url)
   end
+
+  def unavailable_site
+    @unavailable ||= site_for "http://fasdfasdfasd.com"
+  end
+
+  def unknown_site
+    @unknown_site ||= available_site
+  end
+
 end
