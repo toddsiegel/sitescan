9bc2c35bf571b382a02a2306034468811666d1e4
Todd Siegel <todd.siegel@solerasoftware.com>
2013-11-10 11:18:04 -0500

    admittedly naïve implementation

--- lib/sitescan/site.rb
+++ lib/sitescan/site.rb
@@ -1,11 +1,20 @@
 class Site
 
   def initialize(url)
-    
+    @url = url
   end
 
   def available?
-    false
+  begin
+    http = HTTParty.get(url)
+  rescue SocketError
+    return false
   end
+  http.response.is_a? Net::HTTPSuccess
+  end
+
+  private
+
+  attr_reader :url
 
 end
--- spec/site_scan_spec.rb
+++ spec/site_scan_spec.rb
@@ -1,5 +1,6 @@
 require "minitest/spec"
 require "minitest/autorun"
+require "httparty"
 
 require_relative "../lib/sitescan/site"
 
@@ -8,5 +9,10 @@ describe Site do
   it "can tell a site is not avialable" do
     site = Site.new("http://fasdfasdfasd.com")
     site.available?.must_be :== ,false
+  end
+
+  it "can tell a site is available" do
+    site = Site.new("https://github.com")
+    site.available?.must_be :== ,true
   end
 end
